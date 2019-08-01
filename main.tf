425e08538f55f817df878b8be0c863f8b33205c4
root <root@ip-172-31-33-202.ap-northeast-1.compute.internal>
2019-08-01 03:14:34 +0000

    edit resource name

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region     = "ap-northeast-1"
 }
 
-resource "aws_instance" "sample-inst" {
+resource "aws_instance" "sample-ec2" {
     ami           = "ami-0c3fd0f5d33134a76"
     instance_type = "t2.micro"
     monitoring    = false
